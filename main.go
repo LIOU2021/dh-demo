bf76f185ba5a980525173c4e157095c48de09587
admin <admin@admindeMac-mini.local>
2023-08-11 11:55:09 +0800

    feat

--- main.go
+++ main.go
@@ -1,38 +1,15 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
-	"runtime"
-	"time"
-
-	"github.com/panjf2000/ants/v2"
 )
 
-var count = 0
-
-func greeting(i int) func() {
-	return func() {
-		time.Sleep(1 * time.Second)
-		count++
-		fmt.Printf("hello world: %d, goroutine_num: %d\n", i, runtime.NumGoroutine())
-	}
-}
-
 func main() {
-	fmt.Println("start goroutine: ", runtime.NumGoroutine())
-	p, _ := ants.NewPool(5)
-	fmt.Println("first p.Running(): ", p.Running())
-	defer p.Release()
-
-	for i := 0; i < 20; i++ {
-		index := i
-		p.Submit(greeting(index))
-	}
-
-	fmt.Println("later p.Running(): ", p.Running())
-	fmt.Println("end goroutine: ", runtime.NumGoroutine())
-	fmt.Println("count: ", count)
-	for {
-		time.Sleep(1 * time.Second)
-	}
+	data := "https://www.google.com"
+	has := md5.Sum([]byte(data))
+	// hex.EncodeToString(has)
+	mdtStr := fmt.Sprintf("%x", has)
+	fmt.Println(mdtStr)
+	fmt.Println(len(mdtStr))
 }
